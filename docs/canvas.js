7586ec432f870cc3e6cfaf1f1d2a1b25efd1b1bd
lmackenroth <lmackenroth@smu.edu>
2024-11-24 19:47:06 -0600

    attempt 7, big changes, lets see

--- docs/canvas.js
+++ docs/canvas.js
@@ -4,10 +4,12 @@ function project(p5) {
     // This will hold all of my bird instances
     // const birds = [];
     let gradiant;
+    console.log('we made it')
 
     p5.setup = () => {
         // Adjust to the size of the screen dynamically
         p5.createCanvas(1000, 500);
+        console.log('making canvas')
         gradiant = new Gradiant(p5);
     };
 
