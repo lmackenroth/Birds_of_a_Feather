4b369b232c0a253af0d5e319478f46801f4ad3fa
lmackenroth <lmackenroth@smu.edu>
2024-11-24 18:22:58 -0600

    attempt 6 fixing index html,changed to ES module

--- docs/canvas.js
+++ docs/canvas.js
@@ -1,19 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var p5_1 = require("p5");
-//import { Birds } from './project';
-var gradiant_1 = require("../src/gradiant");
+// changed to make it ES modules
+import p5 from "p5"; // Import p5.js
+import Gradiant from "./gradiant.js"; // Import the Gradiant module
+
 function project(p5) {
-    //this will hold all of my bird instances
-    //const birds: Birds[] = [];
-    var gradiant;
-    p5.setup = function () {
-        //figure out how to make it adjust to the of the screen dynamically
+    // This will hold all of my bird instances
+    // const birds = [];
+    let gradiant;
+
+    p5.setup = () => {
+        // Adjust to the size of the screen dynamically
         p5.createCanvas(1000, 500);
-        gradiant = new gradiant_1.default(p5);
+        gradiant = new Gradiant(p5);
     };
-    p5.draw = function () {
+
+    p5.draw = () => {
         gradiant.render(); // Render gradient background
     };
 }
-new p5_1.default(project);
+
+// Initialize the p5 instance
+new p5(project);
